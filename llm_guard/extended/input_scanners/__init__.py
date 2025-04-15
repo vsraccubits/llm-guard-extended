b8f46997ffa44e782842482ce5b12b8bb99d9594
Varun S R <varun@bud.studio>
2025-04-15 06:52:15 +0530

    feat: updated toxicity with inference

---
+++ llm_guard/extended/input_scanners/__init__.py
@@ -0,0 +1,38 @@
+"""Input scanners init"""
+
+# from .anonymize import Anonymize
+# from .ban_code import BanCode
+# from .ban_competitors import BanCompetitors
+# from .ban_substrings import BanSubstrings
+# from .ban_topics import BanTopics
+# from .code import Code
+# from .gibberish import Gibberish
+# from .invisible_text import InvisibleText
+# from .language import Language
+# from .prompt_injection import PromptInjection
+# from .regex import Regex
+# from .secrets import Secrets
+# from .sentiment import Sentiment
+# from .token_limit import TokenLimit
+from .toxicity import ToxicityExtended
+
+# from .util import get_scanner_by_name
+
+__all__ = [
+    # "Anonymize",
+    # "BanCode",
+    # "BanCompetitors",
+    # "BanSubstrings",
+    # "BanTopics",
+    # "Code",
+    # "Gibberish",
+    # "InvisibleText",
+    # "Language",
+    # "PromptInjection",
+    # "Regex",
+    # "Secrets",
+    # "Sentiment",
+    # "TokenLimit",
+    "ToxicityExtended",
+    # "get_scanner_by_name",
+]
